8d437a72f5905a0a1080f5a6c4a9ff408f485d56
yahya ganjo <yahya.ganjo@gmail.com>
2021-07-09 01:34:25 +0200

    change the logo

--- src/Views/renderHeader.js
+++ src/Views/renderHeader.js
@@ -6,7 +6,7 @@ function renderHeader() {
   const appContainer = createAndAppend(document.body, "div", "app-container");
   const header = createAndAppend(appContainer, "div", "header");
   const logo = createAndAppend(header, "img", "logo");
-  logo.src = "public/logo.png";
+  logo.src = "public/logo_1.jpg";
   const navbar = createAndAppend(header, "div", "navbar");
   const home = createElementWithClass(navbar, "button", "nav-btn");
   home.textContent = "Home";
